0330f45e90bc4069272893d5f997ee580b8eac0a
henwist <henwist@gmail.com>
2017-05-06 17:37:39 +0200

    BoundingBox drawn - but not on the right spot.

---
+++ GameEngine/Components/BoundingVolumeComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Components
+{
+    public class BoundingVolumeComponent : Component
+    {
+        public BoundingBox bbox { get; set; }
+        public BoundingSphere bsphere { get; set; }
+    }
+}
